71057ed723de0353800f16f07ef81fab6a58832a
vishwasenthil76 <vishwanathans070@gmail.com>
2025-07-30 23:29:17 +0530

    integrated the faculty overall list but need some more changes

--- src/main/java/com/example/profileservice/controller/FacultyController.java
+++ src/main/java/com/example/profileservice/controller/FacultyController.java
@@ -10,7 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
-
+@CrossOrigin (origins= "http://localhost:3000")
 @RestController
 @RequestMapping("/api/admin/faculty")
 public class FacultyController {
